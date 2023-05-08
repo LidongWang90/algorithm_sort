040df861911c1bf2059346e7693956b6f0496c50
Lidong90 <98975055+Lidong90@users.noreply.github.com>
2023-05-08 14:30:28 -0400

    5/8

--- sort_bubble.py
+++ sort_bubble.py
@@ -3,9 +3,9 @@
 list = [64, 25, 12, 22, 11]
 for i in range(len(list)):
     # compare two ele from the first ele. if left ele
-    # is smaller than the next one, swap, to put the biggest
-    # one into the left end. In next round, continue to compare
-    # except for the left end ele.
+    # is bigger than the next one, swap, to put the biggest
+    # one into the right end. In next round, continue to compare
+
     for j in range(len(list)-i-1):
         if list[j] > list[j+1]:
             list[j], list[j+1] = list[j+1], list[j]
